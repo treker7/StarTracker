be1bcb5d47b956dbfe6f427dd63a4ee01b1ab4dc
Treker Battig <treker.battig@protonmail.com>
2024-05-07 17:14:47 -0400

    Added js build and dev folders

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './js/starTracker.js',
+    entry: './js/dev/starTracker.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'js/build'),
     },
 };
